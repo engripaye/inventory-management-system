64c64d5e80576aecb55bfbaa881d6b1651e9734a
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-01 09:40:49 +0100

    backend inventory service commit by ENGR. IPAYE

---
+++ src/main/java/dev/engripaye/inventorymanagementsystem/model/Inventory.java
@@ -0,0 +1,9 @@
+package dev.engripaye.inventorymanagementsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inventory", )
+public class Inventory {
+}
