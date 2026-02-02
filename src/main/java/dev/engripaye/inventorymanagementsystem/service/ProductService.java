8f3cb5d1b5c4ce91d68870e150a6389da48b46b3
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-02 11:36:48 +0100

    backend service code commit BY ENGR. IPAYE

--- src/main/java/dev/engripaye/inventorymanagementsystem/service/ProductService.java
+++ src/main/java/dev/engripaye/inventorymanagementsystem/service/ProductService.java
@@ -1,8 +1,10 @@
 package dev.engripaye.inventorymanagementsystem.service;
 
+import dev.engripaye.inventorymanagementsystem.dto.ProductRequest;
 import dev.engripaye.inventorymanagementsystem.model.Product;
 import dev.engripaye.inventorymanagementsystem.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,16 @@ public class ProductService {
     public Product createProduct(ProductRequest request){
         Product product = new Product();
         product.setSku(request.getSku());
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setUnitPrice(request.getUnitPrice());
+
+        return productRepository.save(product);
 
     }
+    public Product getProductById(Long id){
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+
 }
