dd72ca55c5ffff2ebbb8854db7d5fd8c1825de4f
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-02 12:47:32 +0100

    backend service code commit BY ENGR. IPAYE

--- src/main/java/dev/engripaye/inventorymanagementsystem/controller/InventoryController.java
+++ src/main/java/dev/engripaye/inventorymanagementsystem/controller/InventoryController.java
@@ -1,5 +1,8 @@
 package dev.engripaye.inventorymanagementsystem.controller;
 
+import dev.engripaye.inventorymanagementsystem.dto.InventoryTransactionRequest;
+import dev.engripaye.inventorymanagementsystem.model.Product;
+import dev.engripaye.inventorymanagementsystem.model.WareHouse;
 import dev.engripaye.inventorymanagementsystem.service.InventoryService;
 import dev.engripaye.inventorymanagementsystem.service.ProductService;
 import dev.engripaye.inventorymanagementsystem.service.WarehouseService;
@@ -7,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +25,21 @@ public class InventoryController {
 
     @PostMapping("/transaction")
     public ResponseEntity<String> processInventoryTransaction(
-            @Valid
-    )
+            @Valid @RequestBody InventoryTransactionRequest request
+            ){
+        Product product = productService.getProductById(request.getProductId());
+        WareHouse wareHouse = warehouseService.getWarehouseByid(request.getWarehouseId());
+
+        inventoryService.processTransaction(
+                product,
+                wareHouse,
+                request.getTransactionType(),
+                request.getQuantity(),
+                request.getReference()
+        );
+
+        return ResponseEntity.ok("Inventory transaction processed successfully ");
+    }
 }
 
 
