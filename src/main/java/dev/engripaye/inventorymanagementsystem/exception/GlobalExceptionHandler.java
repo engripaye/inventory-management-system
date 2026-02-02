e5a443d1032368d27d1c848b637fd89b52863f76
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-02 14:16:02 +0100

    backend system exception handler commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/inventorymanagementsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/engripaye/inventorymanagementsystem/exception/GlobalExceptionHandler.java
@@ -1,10 +1,24 @@
 package dev.engripaye.inventorymanagementsystem.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(
+            ResourceNotFoundException exception
+    ){
+        Map<String, String> response = new HashMap<>();
+        response.put("error", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 
 }
