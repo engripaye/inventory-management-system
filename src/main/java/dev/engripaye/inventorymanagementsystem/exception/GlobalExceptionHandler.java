bf3dd0f37c36179bfb8f3ca40b6977458d9da57e
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-02-02 16:24:03 +0100

    backend system exception handler commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/inventorymanagementsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/engripaye/inventorymanagementsystem/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dev.engripaye.inventorymanagementsystem.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +20,27 @@ public class GlobalExceptionHandler {
         Map<String, String> response = new HashMap<>();
         response.put("error", exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalState(
+            IllegalStateException exception
+    ){
+        Map<String, String> response = new HashMap<>();
+        response.put("error", exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(
+            MethodArgumentNotValidException exception
+    ){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(
+                err -> errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errors);
     }
 
 }
